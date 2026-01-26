a09451af1f2d728588a4bc7f5e7eba5134507209
Yan Chan <fischerbobby865@gmail.com>
2026-01-26 22:27:44 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base : '/energy_project',
     envDir: '../',
     headers: [
       {
