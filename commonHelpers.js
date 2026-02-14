629588ee4eb964f9b9e5117bc26d92f55e687595
unknown <Yan>
2026-02-14 13:55:32 +0200

    Updates

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{i as o,a as t,_ as a}from"./assets/applyAssets-3f067565.js";o().then(()=>(t(),a(()=>import("./assets/favorites-60c007ac.js"),["assets/favorites-60c007ac.js","assets/exercises-right-part-filter-1612d1ec.js","assets/vendor-295897c2.js"]))).catch(r=>console.error("bootstrap-favorites error:",r));
+import{i as a,a as i,_ as s}from"./assets/applyAssets-10e6dd52.js";(async()=>(await a(),i(),await s(()=>import("./assets/main-f9c41eed.js"),["assets/main-f9c41eed.js","assets/vendor-295897c2.js"])))();
 //# sourceMappingURL=commonHelpers.js.map
